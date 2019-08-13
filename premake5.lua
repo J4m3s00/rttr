04ba82e30ab735b0451895f9a9b32e6c23fdb028
J4m3s00 <james.lol2000@gmail.com>
2019-08-13 20:08:45 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,25 @@
+project "lua"
+    kind "StaticLib"
+    language "C++"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    includedirs
+    {
+        ""
+    }
+
+    files
+    {
+        "src/rttr/**.h",
+        "src/rttr/**.cpp"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+        
+    filter { "system:windows", "configurations:Release" }
+buildoptions "/MT"
